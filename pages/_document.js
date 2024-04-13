0a3883f5ad2220c3c6d230aedbc812b6993bf961
Ajitesh13 <aji.yash13@gmail.com>
2024-04-13 17:29:45 +0530

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import theme from '../lib/theme'
 
 export default class Document extends NextDocument {
@@ -12,6 +13,7 @@ export default class Document extends NextDocument {
           <Main />
           <NextScript />
         </body>
+        <GoogleAnalytics gaId="G-8SYFHFEER9" />
       </Html>
     )
   }
