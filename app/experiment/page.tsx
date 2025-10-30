a855f2dfffdcf57e0212b00f6b0400bc98741637
Ajitesh13 <aji.yash13@gmail.com>
2025-10-30 18:44:28 +0530

    add sample deck

---
+++ app/experiment/page.tsx
@@ -0,0 +1,12 @@
+export default function SampleDeck() {
+  return (
+    <div>
+      <iframe
+        src="/sample_deck/index.html"
+        title="Sample Deck"
+        style={{ width: '100%', height: '100vh', border: '0' }}
+        allowFullScreen
+      />
+    </div>
+  )
+}
